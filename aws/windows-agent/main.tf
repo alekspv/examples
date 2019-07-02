9e52eb312475afbd32f991765a2aa6337e0546fc
Kostiantyn Oleksiienko <kostiantyn_oleksiienko@epam.com>
2019-07-02 10:04:17 +0300

    Modified main.tf

--- aws/windows-agent/main.tf
+++ aws/windows-agent/main.tf
@@ -1,95 +1,94 @@
 locals {
-  cluster_name="developmentC1"
-  num_masters                  = "1"
-  num_private_agents           = "1"
-  num_public_agents            = "1"
-  num_winagent                 = "2"
-  dcos_version                 = "1.13.0"
-  dcos_instance_os             = "centos_7.5"
-  bootstrap_instance_type      = "r5.large"
-  masters_instance_type        = "r5.xlarge"
-  private_agents_instance_type = "r5.large"
-  public_agents_instance_type  = "r5.large"
-  ssh_public_key_file          = "~/.ssh/ep3.pub"
-  ssh_private_key_file         = "~/.ssh/ep3"
-  admin_ips                    = ["${data.http.whatismyip.body}/32", "85.223.209.0/24", "87.245.220.0/26", "85.223.141.72/29", "89.162.139.0/27", "91.120.48.0/24", "80.92.226.132/30", "195.56.119.208/28", "195.56.109.192/28", "109.86.106.122/32"]
-  owner                        = "John Dow"
-  expiration                   = "20h"
+  cluster_name                       = "development"
+  num_masters                        = "1"
+  num_private_agents                 = "1"
+  num_public_agents                  = "1"
+  num_winagent                       = "2"
+  dcos_version                       = "1.13.0"
+  dcos_instance_os                   = "centos_7.5"
+  bootstrap_instance_type            = "r5.large"
+  masters_instance_type              = "r5.xlarge"
+  private_agents_instance_type       = "r5.large"
+  public_agents_instance_type        = "r5.large"
+  ssh_public_key_file                = "~/.ssh/id_rsa.pub"
+  ssh_private_key_file               = "~/.ssh/id_rsa"
+  admin_ips                          = ["${data.http.whatismyip.body}/32"]
+  owner                              = "John Doe"
+  expiration                         = "24h"
 }
 
 provider "aws" {
   # Change your default region here
-  region = "us-west-2"
+  region                             = "us-west-2"
 }
 module "dcos" {
-  source  = "dcos-terraform/dcos/aws"
-  version = "~> 0.2.0"
+  source                             = "dcos-terraform/dcos/aws"
+  version                            = "~> 0.2.0"
 
-  cluster_name        = "${local.cluster_name}"
-  ssh_public_key_file = "${local.ssh_public_key_file}"
-  admin_ips           = "${local.admin_ips}"
-  num_masters        = "${local.num_masters}"
-  num_private_agents = "${local.num_private_agents}"
-  num_public_agents  = "${local.num_public_agents}"
+  cluster_name                       = "${local.cluster_name}"
+  ssh_public_key_file                = "${local.ssh_public_key_file}"
+  admin_ips                          = "${local.admin_ips}"
+  num_masters                        = "${local.num_masters}"
+  num_private_agents                 = "${local.num_private_agents}"
+  num_public_agents                  = "${local.num_public_agents}"
 
-  dcos_version = "${local.dcos_version}"
+  dcos_version                       = "${local.dcos_version}"
 
-  dcos_instance_os    = "${local.dcos_instance_os}"
-  bootstrap_instance_type = "${local.bootstrap_instance_type}"
-  masters_instance_type  = "${local.masters_instance_type}"
-  private_agents_instance_type = "${local.private_agents_instance_type}"
-  public_agents_instance_type = "${local.public_agents_instance_type}"
+  dcos_instance_os                   = "${local.dcos_instance_os}"
+  bootstrap_instance_type            = "${local.bootstrap_instance_type}"
+  masters_instance_type              = "${local.masters_instance_type}"
+  private_agents_instance_type       = "${local.private_agents_instance_type}"
+  public_agents_instance_type        = "${local.public_agents_instance_type}"
 
   providers = {
-    aws = "aws"
+    aws                              = "aws"
   }
   tags = {
-    owner = "${local.owner}"
-    expiration = "${local.expiration}"
+    owner                            = "${local.owner}"
+    expiration                       = "${local.expiration}"
   }
   # Enterprise users uncomment this section and comment out below
-  # dcos_variant              = "ee"
-  # dcos_license_key_contents = "${file("./license.txt")}"
+  # dcos_variant                     = "ee"
+  # dcos_license_key_contents        = "${file("./license.txt")}"
   # Make sure to set your credentials if you do not want the default EE
   # dcos_superuser_username          = "superuser-name"
-  # dcos_superuser_password_hash = "${file("./dcos_superuser_password_hash.sha512")}"
+  # dcos_superuser_password_hash     = "${file("./dcos_superuser_password_hash.sha512")}"
 
   # Default is DC/OS
-  dcos_variant = "open"
+  dcos_variant                       = "open"
 }
 
 module "windows-agent" {
   source = "git::https://github.com/alekspv/terraform-aws-windows-instance.git?ref=support/0.2.x"
-  num_winagent = "${local.num_winagent}"
-  admin_ips = "${local.admin_ips}"
-  vpc_id = "${module.dcos.infrastructure.vpc.id}"
-  subnet_id = "${module.dcos.infrastructure.vpc.subnet_ids}"
-  cluster_name = "${local.cluster_name}"
-  expiration = "${local.expiration}"
-  owner = "${local.owner}"
-  aws_key_name = "${module.dcos.infrastructure.aws_key_name}"
-  security_group_admin = "${module.dcos.infrastructure.security_groups.admin}"
-  security_group_internal = "${module.dcos.infrastructure.security_groups.internal}"
-  bootstrap_private_ip = "${module.dcos.infrastructure.bootstrap.private_ip}"
-  bootstrap_public_ip = "${module.dcos.infrastructure.bootstrap.public_ip}"
-  bootstrap_os_user = "${module.dcos.infrastructure.bootstrap.os_user}"
-  ssh_private_key_file = "${local.ssh_private_key_file}"
-  masters_private_ips = "${module.dcos.infrastructure.masters.private_ips}"
+  num_winagent                       = "${local.num_winagent}"
+  admin_ips                          = "${local.admin_ips}"
+  vpc_id                             = "${module.dcos.infrastructure.vpc.id}"
+  subnet_id                          = "${module.dcos.infrastructure.vpc.subnet_ids}"
+  cluster_name                       = "${local.cluster_name}"
+  expiration                         = "${local.expiration}"
+  owner                              = "${local.owner}"
+  aws_key_name                       = "${module.dcos.infrastructure.aws_key_name}"
+  bootstrap_private_ip               = "${module.dcos.infrastructure.bootstrap.private_ip}"
+  bootstrap_public_ip                = "${module.dcos.infrastructure.bootstrap.public_ip}"
+  bootstrap_os_user                  = "${module.dcos.infrastructure.bootstrap.os_user}"
+  ssh_private_key_file               = "${local.ssh_private_key_file}"
+  masters_private_ips                = "${module.dcos.infrastructure.masters.private_ips}"
+  security_groups                    = ["${module.dcos.infrastructure.security_groups.admin}","${module.dcos.infrastructure.security_groups.internal}"]
 }
 
 # Used to determine your public IP for forwarding rules
 data "http" "whatismyip" {
-  url = "http://whatismyip.akamai.com/"
+  url                                = "http://whatismyip.akamai.com/"
 }
 
 output "masters-ips" {
-  value = "${module.dcos.masters-ips}"
+  value                              = "${module.dcos.masters-ips}"
 }
 
 output "cluster-address" {
-  value = "${module.dcos.masters-loadbalancer}"
+  value                              = "${module.dcos.masters-loadbalancer}"
 }
 
 output "public-agents-loadbalancer" {
-  value = "${module.dcos.public-agents-loadbalancer}"
+  value                              = "${module.dcos.public-agents-loadbalancer}"
 }
